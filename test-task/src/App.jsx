acdf6c051c6595c43993a60370d63d4166dfd004
artem-badasyan <133artemka123@gmail.com>
2025-07-22 01:15:32 +0300

    2

--- test-task/src/App.jsx
+++ test-task/src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import './styles/global.scss'
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/infotecstesttask">
+    <HashRouter basename="/infotecstesttask">
       <Routes>
-        <Route element={<HomePage />} path='/'></Route>
+        <Route element={<HomePage />} path="/"></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
